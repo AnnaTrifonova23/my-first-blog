107c6f62ff45ce81d7e33596c1359386e82eb49c
lisaq <lisa@lisaquatmann.com>
2024-10-18 15:36:50 +0200

    updated to what was on pa

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@3$ebfyc01*6x^eu)u9=3hj!j7j!g)_ksekgz3p^uj50&!dzk*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'anna1987.pythonanywhere.com']
 
 
 # Application definition
