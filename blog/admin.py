bb42a7d3160a905b454bfa6888777db0ad16e7ed
AnnaTrifonova23 <atechnoreva@gmail.com>
2025-05-20 12:30:01 +0200

    I created comment model.

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from .models import Post, Comment
 from .models import Post
 
 
 admin.site.register(Post)
-
+admin.site.register(Comment)
 
 # Register your models here.
