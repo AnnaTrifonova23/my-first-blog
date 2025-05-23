8acd6cad955ea268813f43129d6f10c152a34ae6
AnnaTrifonova23 <atechnoreva@gmail.com>
2025-05-23 17:04:38 +0200

    I added the def to the class Post.

--- blog/models.py
+++ blog/models.py
@@ -11,12 +11,17 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='blog_images/', blank=True, null=True)  
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
+
     
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
